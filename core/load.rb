9baf9d76f3c766b90c4ff8180858f5160da867c2
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-07-28 12:12:06 +0200

    Fixed file loading in mruby-require.

--- core/load.rb
+++ core/load.rb
@@ -1,14 +1,14 @@
 #
 # core/load.rb
 #   Entry script for Moon's mrb
-require './core/state.rb'
-require './core/mouse.rb'
-require './core/music_activity_hud.rb'
-require './core/state_key_hold_test.rb'
-require './core/state_modifier_test.rb'
-require './core/state_moon_player.rb'
-require './core/state_music_activity.rb'
-require './core/state_test.rb'
+require 'core/state'
+require 'core/mouse'
+require 'core/music_activity_hud'
+require 'core/state_key_hold_test'
+require 'core/state_modifier_test'
+require 'core/state_moon_player'
+require 'core/state_music_activity'
+require 'core/state_test'
 
 #State.push(State_KeyHoldTest)
 State.push(State_ModifierTest)
