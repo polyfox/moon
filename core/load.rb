f743808178ee4f3b18dcc4ccb37433204a7ba953
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-07-30 17:21:33 +0200

    Add a Shader class.
    It actually represents a shader program (vertex + fragment shader,
    or only one of those). Later, this will be suppliable per-model.

--- core/load.rb
+++ core/load.rb
@@ -5,13 +5,13 @@ require 'core/state'
 require 'core/mouse'
 require 'core/music_activity_hud'
 require 'core/state_key_hold_test'
-require 'core/state_modifier_test'
+#require 'core/state_modifier_test'
 require 'core/state_moon_player'
 require 'core/state_music_activity'
 require 'core/state_test'
 
 #State.push(State_KeyHoldTest)
-State.push(State_ModifierTest)
+#State.push(State_ModifierTest)
 #State.push(State_MoonPlayer)
 #State.push(State_MusicActivity)
-#State.push(State_Test)
+State.push(State_Test)
