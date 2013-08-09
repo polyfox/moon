650747095f69c148b85fa4bb71b9e4091c17cf2a
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-08-09 19:19:40 +0200

    move round(double) in platform.c to platform.h

--- vendor/freetype-gl/platform.h
+++ vendor/freetype-gl/platform.h
@@ -36,26 +36,18 @@
 
 #include <stdlib.h>
 
-//-------------------------------------------------
-// stdint.h is not available on VS2008 or lower
-//-------------------------------------------------
-#ifdef _MSC_VER
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-#else
-#include <stdint.h>
-#endif // _MSC_VER
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if defined(_WIN32) || defined(_WIN64) 
-    /* does not exist on windows */
-    double round (float v);
-#   pragma warning (disable: 4244) // suspend warnings
+#if defined(_WIN32) || defined(_WIN64)
+#include <math.h>
+
+/* does not exist on windows */
+double round (float v) {
+  return floor(v+0.5f);
+}
+# pragma warning (disable: 4244) // suspend warnings
 #endif // _WIN32 || _WIN64
 
 #ifdef __cplusplus
