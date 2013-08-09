297a92714719e271e8aaacb9bed42eca9505a1cc
IceDragon200 <mistdragon100@gmail.com>
2013-08-08 19:02:02 -0500

    Tilemap multi layer support
    Big change to the Tilemap class, instead of a Table,
    Tilemap now uses a DataMatrix for its data to support multiple
    layers.
    
    The rendering was tweaked to support the new layer, and
    a bit of optimization was done for tile_ids less than 0
    (which are considered invisible)

--- core/tilemap.rb
+++ core/tilemap.rb
@@ -5,7 +5,7 @@
 class Tilemap
 
   attr_accessor :tileset # Spritesheet
-  attr_accessor :data    # Table
+  attr_accessor :data    # DataMatrix
 
   ##
   # initialize
@@ -17,26 +17,46 @@ class Tilemap
 
   ##
   # render(int x, int y, int z, int ox, int oy, int width, int height)
-  #   (x), (y), (z) are the positions in which this Tilemap should be render on screen
-  #   (ox), (oy) is the data offset (where should the tilemap#data start from)
-  #   (width), (height) is the render size in cells of the tilemap
-  def render(x, y, z, ox, oy, width, height)
+  #   (x), (y), (z)
+  #     is the position (in pixels), for the Tilemap to be rendered
+  #   (ox), (oy), (oz)
+  #     is the data offset (where should the Tilemap#data start from)
+  #   (width), (height), (layers)
+  #     is the number of cells to render, and the number of layers to be rendered
+  def render(x, y, z, ox, oy, oz, width, height, layers)
     return unless @data
     return unless @tileset
     cell_width  = @tileset.cell_width
     cell_height = @tileset.cell_height
-    for i in 0...height
-      for j in 0...width
-        dx = j + ox
-        dy = i + oy
+    # we loop by layer
+    layers.times do |l|
+      dz = l + oz # offset data z index
+      if @loop_data
+        dz %= @data.zsize
+      else
+        next if dz < 0 || dz >= @data.zsize
+      end
+      # and then by row
+      height.times do |i|
+        dy = i + oy # offset data y index
         if @loop_data
-          dx %= @data.xsize
           dy %= @data.ysize
         else
-          next if dx < 0 || dy < 0 || dx >= @data.xsize || dy >= @data.ysize
+          next if dy < 0 || dy >= @data.ysize
         end
-        tile_id = @data[dx, dy]
-        @tileset.render(x + j * cell_width, y + i * cell_height, z, tile_id)
+        # and then render by cell
+        width.times do |j|
+          dx = j + ox # offset data x index
+          if @loop_data
+            dx %= @data.xsize
+          else
+            next if dx < 0 || dx >= @data.xsize
+          end
+          tile_id = @data[dx, dy, dz]
+          next if tile_id < 0 # if -1 or less, then its a negative tile and therefore should not be rendered
+          # TODO add a @data_zmap, to have custom z heights in rendering
+          @tileset.render(x + j * cell_width, y + i * cell_height, z, tile_id)
+        end
       end
     end
   end
