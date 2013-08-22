1c9904cfe323e1726a60caacf546ed4f11904d82
IceDragon200 <mistdragon100@gmail.com>
2013-08-22 10:44:00 -0500

    Moved State_ModifierTest::BUTTONS to Input::Mouse
    BUTTONS was in the wrong place to begin with.
    With this, it should be easier to enumerate the Mouse buttons, to
    test all of them for input if necessary.

--- core/mouse.rb
+++ core/mouse.rb
@@ -1,5 +1,19 @@
-class Input
+module Input
   module Mouse
+
+    BUTTONS = [
+      Buttons::BUTTON_1,
+      Buttons::BUTTON_2,
+      Buttons::BUTTON_3,
+      Buttons::BUTTON_4,
+      Buttons::BUTTON_5,
+      Buttons::BUTTON_6,
+      Buttons::BUTTON_7,
+      Buttons::BUTTON_8
+    ]
+
+    MODS = [0, Input::Keyboard::Keys::MOD_SHIFT, Input::Keyboard::Keys::MOD_CONTROL,
+               Input::Keyboard::Keys::MOD_ALT, Input::Keyboard::Keys::MOD_SUPER]
 
     def self.pos
       [x, y]
@@ -8,7 +22,7 @@ class Input
     def self.in_area?(x, y, width, height)
       self.x.between?(x, x+width) && self.y.between?(y, y+height)
     end
- 
+
     def self.in_rect?(rect)
       in_area?(rect.x, rect.y, rect.width, rect.height)
     end
