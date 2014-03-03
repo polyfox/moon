8a2bd1fc1b349bc2c30a8d72865c6306402f399e
IceDragon200 <mistdragon100@gmail.com>
2014-03-03 13:52:18 -0500

    Fixed texture
    Fixed Texture's displaying a repeated texture edge instead of transparency.

--- src/texture.cxx
+++ src/texture.cxx
@@ -9,13 +9,17 @@ namespace Moon {
 
     unsigned char* pixels;
     int channels;
+    float border_color[4] = { 0.0, 0.0, 0.0, 0.0 };
 
     pixels = SOIL_load_image(filename.c_str(), &texture_width, &texture_height, &channels, SOIL_LOAD_AUTO);
     texture_id = SOIL_create_OGL_texture(pixels, texture_width, texture_height, channels, SOIL_CREATE_NEW_ID, SOIL_FLAG_MULTIPLY_ALPHA);
 
     glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border_color);
     glBindTexture(GL_TEXTURE_2D, 0);
   };
 
