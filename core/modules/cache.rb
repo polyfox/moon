cebf8e824afc290e64b664c0979a00fcc6127f1e
IceDragon200 <mistdragon100@gmail.com>
2014-04-07 20:47:51 -0500

    Added Cache::clear

--- core/modules/cache.rb
+++ core/modules/cache.rb
@@ -8,13 +8,22 @@ module Cache
 
   extend self
 
+  def clear(branch_name=nil)
+    if branch_name
+      if cache = @cache[branch_name]
+        cache.clear
+      end
+    else
+      @cache.clear
+    end
+  end
+
   def branch(branch_name)
     define_method(branch_name) do |name, *args|
       key = [name, *args]
       (@cache[branch_name] ||= {})[key] ||= @constructor[branch_name][name].(*args)
     end
-    hsh = @constructor[branch_name] ||= {}
-    yield hsh
+    yield (@constructor[branch_name] ||= {})
   end
 
   class << self
