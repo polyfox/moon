b365083c0e9c087305fd707278897babac93a1e4
IceDragon200 <mistdragon100@gmail.com>
2014-04-11 01:28:35 -0500

    Moved ChunkFlag from ES to Tilemap

--- core/tilemap.rb
+++ core/tilemap.rb
@@ -4,8 +4,25 @@
 # This may need to be rewritten in C/++
 class Tilemap
 
+  module DataFlag
+
+    NONE            = 0        # 0000 0000 # no flag
+    # up and down offsets will conflict, left and right offsets will conflict
+    OFF_UP          = 16       # 0001 0000 # enable offset up
+    OFF_LEFT        = 32       # 0010 0000 # enable offset left
+    OFF_RIGHT       = 64       # 0100 0000 # enable offset right
+    OFF_DOWN        = 128      # 1000 0000 # enable offset down
+    # note that offset will overwrite each other
+    QUART_OFF_TILE  = 1        # 0000 0001 # enable quater offset
+    HALF_OFF_TILE   = 1|2      # 0000 0011 # enable half offset
+    TQUART_OFF_TILE = 1|2|4    # 0000 0111 # enable 3/4 offset
+    FULL_OFF_TILE   = 1|2|4|8  # 0000 1111 # enable full offset
+
+  end
+
   attr_accessor :tileset    # Spritesheet
   attr_accessor :data       # DataMatrix
+  attr_accessor :flags      # DataMatrix
   attr_accessor :data_zmap  # DataMatrix
   attr_accessor :repeat_map # Boolean
   attr_accessor :view       # Cuboid
@@ -15,13 +32,14 @@ class Tilemap
   def initialize
     @tileset    = nil
     @data       = nil
+    @flags      = nil
     @data_zmap  = nil
     @repeat_map = false
     @view       = nil
     yield self if block_given?
   end
 
-  ##
+  ###
   # render(int x, int y, int z, int ox, int oy, int width, int height)
   #   (x), (y), (z)
   #     is the position (in pixels), for the Tilemap to be rendered
@@ -29,6 +47,7 @@ class Tilemap
   #     is the data offset (where should the Tilemap#data start from)
   #   (width), (height), (layers)
   #     is the number of cells to render, and the number of layers to be rendered
+  ###
   def render(x, y, z)
     return unless @data
     return unless @tileset
@@ -80,10 +99,18 @@ class Tilemap
           next if tile_id < 0
 
           zm = @data_zmap ? @data_zmap[dx, dy, dz] : 0
+          flag = @flags ? @flags[dx, dy, dz] : 0
 
-          @tileset.render x + j * cell_width,
-                          y + i * cell_height,
-                          z + zm, tile_id
+          if flag > 0
+            @tileset.render x + j * cell_width,
+                            y + i * cell_height,
+                            z + zm, tile_id
+          else
+            @tileset.render x + j * cell_width,
+                            y + i * cell_height,
+                            z + zm, tile_id
+          end
+
         end
       end
     end
