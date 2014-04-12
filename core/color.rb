a08c17d1e06b3a5f624be1a1f66ed36b7657b213
IceDragon200 <mistdragon100@gmail.com>
2014-04-12 14:44:12 -0500

    Added Color#set, and fixed missing shorthand alpha

--- core/color.rb
+++ core/color.rb
@@ -1,15 +1,22 @@
 module Moon
   class Color
 
-    alias :r :red
+    alias :r  :red
     alias :r= :red=
-    alias :g :green
+    alias :g  :green
     alias :g= :green=
-    alias :b :blue
+    alias :b  :blue
     alias :b= :blue=
+    alias :a  :alpha
+    alias :a= :alpha=
+
+    def set(*args)
+      r, g, b, a = *Color.obj_to_color_a(args.size > 1 ? args : args.first)
+      self.r, self.g, self.b, self.a = r.to_i, g.to_i, b.to_i, a.to_i
+    end
 
     def dup
-      Color.new red, green, blue, alpha
+      Color.new r, g, b, a
     end
 
     def to_a
@@ -18,6 +25,27 @@ module Moon
 
     def to_h
       { r: r, g: g, b: b, a: a }
+    end
+
+    def self.obj_to_color_a(obj)
+      case obj
+      when Moon::Color
+        return *obj
+      when Array
+        case obj.size
+        when 3
+          return *obj, 255
+        # x, y, w, h
+        when 4 then
+          return *obj
+        else
+          raise ArgumentError,
+                "wrong Array size #{obj.size} (expected 3 or 4)"
+        end
+      else
+        raise TypeError,
+              "wrong argument type #{obj.class.inspect} (expected Color or Array)"
+      end
     end
 
   end
