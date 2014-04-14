377a3fd6e3cb5a5c4be74e869a9461ebf774fed0
IceDragon200 <mistdragon100@gmail.com>
2014-04-14 17:28:01 -0500

    Fixed Rect, used initialize instead of new

--- src/ext/mrb_rectangle.cxx
+++ src/ext/mrb_rectangle.cxx
@@ -12,13 +12,16 @@ namespace Moon {
     "Rect", moon_mrb_rectangle_deallocate,
   };
 
-  static mrb_value moon_mrb_rectangle_new(mrb_state *mrb, mrb_value klass) {
+  static mrb_value moon_mrb_rectangle_initialize(mrb_state *mrb, mrb_value self) {
     mrb_int x, y, w, h;
     mrb_get_args(mrb, "iiii", &x, &y, &w, &h);
 
     auto rectangle = new std::shared_ptr<Rect>(new Rect(x, y, w, h));
 
-    return mrb_obj_value(Data_Wrap_Struct(mrb, mrb_class_ptr(klass), &rectangle_data_type, rectangle));
+    DATA_TYPE(self) = &rectangle_data_type;
+    DATA_PTR(self) = rectangle;
+
+    return mrb_nil_value();
   };
 
   static mrb_value moon_mrb_rectangle_x_setter(mrb_state *mrb, mrb_value self) {
@@ -106,15 +109,15 @@ namespace Moon {
     rectangle_class = mrb_define_class_under(mrb, mrb_module_get(mrb, "Moon"), "Rect", mrb->object_class);
     MRB_SET_INSTANCE_TT(rectangle_class, MRB_TT_DATA);
 
-    mrb_define_class_method(mrb, rectangle_class, "new", moon_mrb_rectangle_new,            MRB_ARGS_REQ(4));
-    mrb_define_method(mrb, rectangle_class, "x=",        moon_mrb_rectangle_x_setter,       MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, rectangle_class, "x",         moon_mrb_rectangle_x_getter,       MRB_ARGS_NONE());
-    mrb_define_method(mrb, rectangle_class, "y=",        moon_mrb_rectangle_y_setter,       MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, rectangle_class, "y",         moon_mrb_rectangle_y_getter,       MRB_ARGS_NONE());
-    mrb_define_method(mrb, rectangle_class, "width=",    moon_mrb_rectangle_width_setter,   MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, rectangle_class, "width",     moon_mrb_rectangle_width_getter,   MRB_ARGS_NONE());
-    mrb_define_method(mrb, rectangle_class, "height=",   moon_mrb_rectangle_height_setter,  MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, rectangle_class, "height",    moon_mrb_rectangle_height_getter,  MRB_ARGS_NONE());
+    mrb_define_method(mrb, rectangle_class, "initialize", moon_mrb_rectangle_initialize,     MRB_ARGS_REQ(4));
+    mrb_define_method(mrb, rectangle_class, "x=",         moon_mrb_rectangle_x_setter,       MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, rectangle_class, "x",          moon_mrb_rectangle_x_getter,       MRB_ARGS_NONE());
+    mrb_define_method(mrb, rectangle_class, "y=",         moon_mrb_rectangle_y_setter,       MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, rectangle_class, "y",          moon_mrb_rectangle_y_getter,       MRB_ARGS_NONE());
+    mrb_define_method(mrb, rectangle_class, "width=",     moon_mrb_rectangle_width_setter,   MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, rectangle_class, "width",      moon_mrb_rectangle_width_getter,   MRB_ARGS_NONE());
+    mrb_define_method(mrb, rectangle_class, "height=",    moon_mrb_rectangle_height_setter,  MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, rectangle_class, "height",     moon_mrb_rectangle_height_getter,  MRB_ARGS_NONE());
 
     return rectangle_class;
   };
