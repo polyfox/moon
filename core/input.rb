3cc94df337b68f5272eca5c63415a8c25643e2b7
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-04-19 21:38:15 +0200

    Input on a diet.
    Input is now evented and passes a callback into rubyland.
    Things still need fixing, but this now effectively means we're
    not focusing on having both a C++ and mruby API at the same time.

--- core/input.rb
+++ core/input.rb
@@ -1,35 +1,19 @@
 module Moon
   module Input
 
-    TRIGGERED_THRESHOLD = 0.020
-
-    module Keyboard
-
-      def self.triggered?(key_id, *args)
-        val = (pressed?(key_id, *args) || repeated?(key_id, *args))
-        val && val < Moon::Input::TRIGGERED_THRESHOLD
-      end
-
-      def self.held?(key_id, *args)
-        pressed?(key_id, *args) || repeated?(key_id, *args)
-      end
-
+    def self.on_key(key, scancode, action, mods)
     end
+
+    def self.on_button(button, action, mods)
+    end
+
     module Mouse
 
-      BUTTONS = [
-        Buttons::BUTTON_1,
-        Buttons::BUTTON_2,
-        Buttons::BUTTON_3,
-        Buttons::BUTTON_4,
-        Buttons::BUTTON_5,
-        Buttons::BUTTON_6,
-        Buttons::BUTTON_7,
-        Buttons::BUTTON_8
-      ]
+      BUTTONS = [ BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5,
+                  BUTTON_6, BUTTON_7, BUTTON_8 ]
 
-      MODS = [0, Input::Keyboard::Keys::MOD_SHIFT, Input::Keyboard::Keys::MOD_CONTROL,
-                 Input::Keyboard::Keys::MOD_ALT, Input::Keyboard::Keys::MOD_SUPER]
+      MODS = [0, Input::Keyboard::MOD_SHIFT, Input::Keyboard::MOD_CONTROL,
+                 Input::Keyboard::MOD_ALT, Input::Keyboard::MOD_SUPER]
 
       def self.in_area?(x, y, width, height)
         self.x.between?(x, x+width) && self.y.between?(y, y+height)
@@ -39,17 +23,8 @@ module Moon
         in_area?(rect.x, rect.y, rect.width, rect.height)
       end
 
-      def self.triggered?(key_id, *args)
-        val = pressed?(key_id, *args)
-        val && val < Moon::Input::TRIGGERED_THRESHOLD
-      end
-
       def self.pos
         Vector2.new x, y
-      end
-
-      class << self
-        alias :held? :pressed?
       end
 
     end
