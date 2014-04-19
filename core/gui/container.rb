25c69b1cea005ba74f9ad1410f24e859383d587b
IceDragon200 <mistdragon100@gmail.com>
2014-04-19 01:02:04 -0500

    Moved screen_to_relative and others to ScreenElement mixin module

--- core/gui/container.rb
+++ core/gui/container.rb
@@ -3,6 +3,7 @@ require 'core/gui/observer'
 class Container < Moon::Rect
 
   include Eventable
+  include ScreenElement
 
   attr_accessor :draggable
   attr_accessor :widgets
@@ -119,43 +120,6 @@ class Container < Moon::Rect
 
   def ry2
     ry + width
-  end
-
-  ###
-  # Convert a screen position to a relative position in the Container
-  # @overload screen_to_relative(x, y)
-  # @overload screen_to_relative(vec2)
-  # @return [Vector2]
-  ###
-  def screen_to_relative(*args)
-    vec2 = Vector2[*args]
-    vec2.x -= x
-    vec2.y -= y
-    vec2
-  end
-
-  ###
-  # Convert a relative position in the Container to a screen position
-  # @overload relative_to_screen(x, y)
-  # @overload relative_to_screen(vec2)
-  # @return [Vector2]
-  ###
-  def relative_to_screen(*args)
-    vec2 = Vector2[*args]
-    vec2.x += x
-    vec2.y += y
-    vec2
-  end
-
-  ###
-  # Determines if position is inside the Container
-  # @overload pos_inside?(x, y)
-  # @overload pos_inside?(vec2)
-  # @return [Boolean]
-  ###
-  def pos_inside?(*args)
-    px, py = *Vector2.obj_to_vec2_a(args.size > 1 ? args : args.first)
-    px.between?(x, x2) && py.between?(y, y2)
   end
 
 end
