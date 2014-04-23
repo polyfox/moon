17d33a2c882f850428a87df5a533aaaf5ae5b4f3
IceDragon200 <mistdragon100@gmail.com>
2014-04-23 12:52:33 -0500

    Module.const_get now properly resolves "::Klass" paths

--- core/core_ext/module.rb
+++ core/core_ext/module.rb
@@ -2,8 +2,18 @@ class Module
 
   alias :__const_get__ :const_get
 
+  ###
+  # const_get resolves namespaces and top level constants!
+  # @param [String] path
+  ###
   def const_get(path)
-    path.split("::").inject(Object) { |klass, name| klass.__const_get__(name) }
+    top = self
+    paths = path.split("::")
+    if path.start_with?("::")
+      top = Object
+      paths.shift
+    end
+    paths.inject(top) { |klass, name| klass.__const_get__(name) }
   end
 
 end
