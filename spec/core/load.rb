12d360f337aadbfd9738556dbb0a86fa81761dcb
IceDragon200 <mistdragon100@gmail.com>
2014-06-16 23:13:31 -0500

    Added specs
    No, you can't run it with rspec, the script must be launched from the spec directory.
    script = run_specs

---
+++ spec/core/load.rb
@@ -0,0 +1,5 @@
+$: << "../"
+require "core/micro-spec"
+require "core/micro-assert"
+#require "../vendor/mruby/test/assert"
+require "../core/load.rb"
---
+++ spec/scripts/load.rb
@@ -0,0 +1,9 @@
+require "vector2_spec"
+require "vector3_spec"
+require "vector4_spec"
+
+run_specs
+
+exit
+
+State.push State
