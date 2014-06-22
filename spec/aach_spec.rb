83228c6b9fef59c86c5876a87cc3050d994e3180
IceDragon200 <mistdragon100@gmail.com>
2014-06-22 00:18:29 -0500

    Added Aach, a palette parsing module
    This may be dropped from Moon, and moved to ES instead, unless it becomes widely used.

---
+++ spec/aach_spec.rb
@@ -0,0 +1,27 @@
+describe Moon::Aach do
+  it "should load a hex.RGB palette" do
+    Aach.load_palette(
+      "format" => "hex.RGB",
+      "colors" => {
+        "black" => "000",
+        "white" => "FFF",
+        "blue" => "00F",
+        "red" => "F00",
+        "green" => "0F0"
+      }
+    )
+  end
+
+  it "should load a hex.RRGGBB palette" do
+    Aach.load_palette(
+      "format" => "hex.RRGGBB",
+      "colors" => {
+        "black" => "000000",
+        "white" => "FFFFFF",
+        "blue" => "0000FF",
+        "red" => "FF0000",
+        "green" => "00FF00"
+      }
+    )
+  end
+end
