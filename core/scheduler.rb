21b787431659409b0adac4864d68ff0d5e52b3b5
IceDragon200 <mistdragon100@gmail.com>
2014-07-15 18:50:20 -0500

    Scheduler can now parse duration strings (eg. 1s 2m 3d)

--- core/scheduler.rb
+++ core/scheduler.rb
@@ -5,13 +5,56 @@ module Moon
     attr_accessor :tasks
     attr_accessor :intervals
 
+    ###
+    # @param [String] str
+    # @return [Float] duration  in seconds
+    ###
+    def self.parse_duration(str)
+      #   - milliseconds
+      # s - seconds
+      # m - minutes
+      # h - hours
+      # d - days
+      # w - weeks
+      # M - months
+      # y - years
+      # Now lets be honest here, who would be running this thing for more than
+      # a few hours anyway...
+      value = 0.0
+      str.scan(/(\d+|\d+\.\d+)([smhdwMy])?/).each do |a|
+        v = a[0].to_i
+        letter = a[1].to_s
+        p [v, letter]
+        case letter
+        when ""
+          value += v / 1000.0
+        when "s"
+          value += v
+        when "m"
+          value += v * 60.0
+        when "h"
+          value += v * 60.0 * 60.0
+        when "d"
+          value += v * 60.0 * 60.0 * 24.0
+        when "w"
+          value += v * 60.0 * 60.0 * 24.0 * 7.0
+        when "M"
+          value += v * 60.0 * 60.0 * 24.0 * 7.0 * 30.0
+        when "y"
+          value += v * 60.0 * 60.0 * 24.0 * 7.0 * 30.0 * 12.0
+        end
+      end
+      value
+    end
+
     def initialize
       @tasks = []
       @intervals = []
     end
 
-    def add_interval(*args, &block)
-      interval = Interval.new(*args, &block)
+    def add_interval(duration, &block)
+      duration = self.class.parse_duration(duration) if duration.is_a?(String)
+      interval = Interval.new(duration, &block)
       @intervals.push interval
       interval
     end
@@ -37,11 +80,15 @@ module Moon
     # @return [Timeout]
     ###
     def add_task(duration, &block)
-      timeout = Timeout.new duration, &block
+      duration = self.class.parse_duration(duration) if duration.is_a?(String)
+      timeout = Timeout.new(duration, &block)
       @tasks.push timeout
       timeout
     end
 
+    ###
+    # Clears all tasks
+    ###
     def clear_tasks
       @tasks.clear
     end
