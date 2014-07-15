fd6b50a30a3a79e0f478ad9b2ddb690f1ace8c2f
IceDragon200 <mistdragon100@gmail.com>
2014-07-15 17:26:30 -0500

    core/core > core/animation

---
+++ core/animation.rb
@@ -0,0 +1,4 @@
+require 'core/animation/easer'
+require 'core/animation/interval'
+require 'core/animation/timeout'
+require 'core/animation/transition'
--- core/core.rb
+++
@@ -1,4 +0,0 @@
-require 'core/core/easer'
-require 'core/core/interval'
-require 'core/core/timeout'
-require 'core/core/transition'
