e1f8433be790c6c1ad413ae0c007c54c0ce0d1cc
IceDragon200 <mistdragon100@gmail.com>
2014-07-17 15:28:11 -0500

    Clearer DataModel::Field error messages
    Field will now give proper error messages for invalid Array contents.

--- core/data_model/field.rb
+++ core/data_model/field.rb
@@ -16,31 +16,49 @@ module Moon
         @default.is_a?(Proc) ? @default.call(@type, selfie) : @default
       end
 
-      def check_type(key, value)
-        # validate that obj is an Array of type
-        if @type.is_a?(Array)
-          unless value.is_a?(Array) || (allow_nil && (value == nil))
-            raise TypeError,
-                  "[#{key}] wrong type #{value.class.inspect} (expected Array)"
-          end
-          unless value.all? { |o| @type.any? { |t| o.is_a?(t) } }
+      def check_array_type(key, value)
+        unless value.is_a?(Array)
+          raise TypeError,
+                "[#{key}] wrong type #{value.class.inspect} (expected Array)"
+        end
+        value.each_with_index do |obj, i|
+          unless @type.any? { |t| obj.is_a?(t) }
             str = @type.map { |s| s.inspect }.join(", ")
             raise TypeError,
-                  "[#{key}[]] wrong type #{value.class.inspect} (expected #{str})"
+                  "[#{key}[#{i}]] wrong content type #{obj.class.inspect} (expected #{str})"
           end
+        end
+      end
+
+      def check_hash_type(key, value)
+        unless value.is_a?(Hash)
+          raise TypeError,
+                "[#{key}] wrong type #{value.class.inspect} (expected Hash)"
+        end
+      end
+
+      def check_normal_type(key, value)
+        unless value.is_a?(type)
+          raise TypeError,
+                "[#{key}] wrong type #{value.class.inspect} (expected #{@type.inspect})"
+        end
+      end
+
+      def check_type(key, value)
+        if allow_nil && value.nil?
+          return
+        elsif !allow_nil && value.nil?
+          raise TypeError, ":#{key} shall not be nil"
+        end
+        # validate that obj is an Array and contains correct types
+        if @type.is_a?(Array)
+          check_array_type(key, value)
         # validate that value is a Hash of key type and value type
         elsif @type.is_a?(Hash)
-          unless value.is_a?(Hash) || (allow_nil && (value == nil))
-            raise TypeError,
-                  "[#{key}] wrong type #{value.class.inspect} (expected Hash)"
-          end
-
+          check_hash_type(key, value)
         # validate that value is of type
         elsif @type.is_a?(Module)
-          unless value.is_a?(type) || (allow_nil && (value == nil))
-            raise TypeError,
-                  "[#{key}] wrong type #{value.class.inspect} (expected #{@type.inspect})"
-          end
+          check_normal_type(key, value)
         end
       end
 
