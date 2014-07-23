fdc419728e32c69bfe34a8c3caf64939e656a3e4
IceDragon200 <mistdragon100@gmail.com>
2014-07-23 16:08:37 -0500

    DataModel::Base#id is a String

--- core/data_model/base.rb
+++ core/data_model/base.rb
@@ -1,7 +1,7 @@
 module Moon
   module DataModel
     class Base < Metal
-      field :id,   type: Integer,          default: 0         # ID
+      field :id,   type: String,           default: proc {Random.random.base64(16)} # ID
       field :name, type: String,           default: proc {""} # Name of this model
       field :note, type: String,           default: proc {""} # A string for describing this DataModel
       field :tags, type: [String],         default: proc {[]} # Used for lookups
