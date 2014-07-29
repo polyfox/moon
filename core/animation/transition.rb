46750ca2cfd12a402c444cbfb166f06a163a6194
IceDragon200 <mistdragon100@gmail.com>
2014-07-29 12:14:47 -0500

    namespace Transition in Moon

--- core/animation/transition.rb
+++ core/animation/transition.rb
@@ -1,64 +1,66 @@
-class Transition
-  ### class variables
-  @@id = 0
+module Moon
+  class Transition
+    ### class variables
+    @@id = 0
 
-  ### instance attributes
-  attr_accessor :key
-  attr_accessor :easer
-  attr_accessor :src
-  attr_accessor :dest
-  attr_reader :id
-  attr_reader :time
-  attr_reader :duration
+    ### instance attributes
+    attr_accessor :key
+    attr_accessor :easer
+    attr_accessor :src
+    attr_accessor :dest
+    attr_reader :id
+    attr_reader :time
+    attr_reader :duration
 
-  ###
-  # initialize(src, dest, duration) { |value| transition_callback }
-  # @param [Object] src
-  # @param [Object] dest
-  # @param [Float] duration  in seconds
-  # @return [Void]
-  ###
-  def initialize(src, dest, duration, &block)
-    @id = @@id += 1
-    @key = nil
-    @src = src
-    @dest = dest
-    @time = 0.0
-    @duration = duration
-    @easer = Easing::Linear
-    @callback = block
-  end
+    ###
+    # initialize(src, dest, duration) { |value| transition_callback }
+    # @param [Object] src
+    # @param [Object] dest
+    # @param [Float] duration  in seconds
+    # @return [Void]
+    ###
+    def initialize(src, dest, duration, &block)
+      @id = @@id += 1
+      @key = nil
+      @src = src
+      @dest = dest
+      @time = 0.0
+      @duration = duration
+      @easer = Easing::Linear
+      @callback = block
+    end
 
-  ###
-  # @return [Boolean]
-  ###
-  def done?
-    @time >= @duration
-  end
+    ###
+    # @return [Boolean]
+    ###
+    def done?
+      @time >= @duration
+    end
 
-  ###
-  # @return [Void]
-  ###
-  def refresh
-    @callback.call(@src + @easer.call(@dest - @src) * @time / @duration)
-  end
+    ###
+    # @return [Void]
+    ###
+    def refresh
+      @callback.call(@src + @easer.call(@dest - @src) * @time / @duration)
+    end
 
-  ###
-  # @param [Float] delta
-  # @return [Void]
-  ###
-  def update(delta)
-    return if done?
-    @time += delta
-    @time = @duration if @time > @duration
-    refresh
-  end
+    ###
+    # @param [Float] delta
+    # @return [Void]
+    ###
+    def update(delta)
+      return if done?
+      @time += delta
+      @time = @duration if @time > @duration
+      refresh
+    end
 
-  ###
-  # @return [Void]
-  ###
-  def finish
-    @time = @duration
-    refresh
+    ###
+    # @return [Void]
+    ###
+    def finish
+      @time = @duration
+      refresh
+    end
   end
 end
