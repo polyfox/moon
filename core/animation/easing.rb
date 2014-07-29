5064f397ed5721fbcd3c7de6770b2d440731d749
IceDragon200 <mistdragon100@gmail.com>
2014-07-29 12:53:33 -0500

    Code Gardening, removing excess spaces from brackets

--- core/animation/easing.rb
+++ core/animation/easing.rb
@@ -30,10 +30,10 @@ module Moon
     end
 
     CubicInOut = lambda do |k|
-      if ( k *= 2 ) < 1
+      if (k *= 2) < 1
         0.5 * k * k * k
       else
-        0.5 * ( ( k -= 2 ) * k * k + 2 )
+        0.5 * ((k -= 2) * k * k + 2)
       end
     end
 
@@ -42,14 +42,14 @@ module Moon
     end
 
     QuarticOut = lambda do |k|
-      1 - ( (k-=1) * k * k * k )
+      1 - ((k-=1) * k * k * k)
     end
 
     QuarticInOut = lambda do |k|
-      if ( ( k *= 2 ) < 1)
+      if (k *= 2) < 1
         0.5 * k * k * k * k
       else
-        -0.5 * ( ( k -= 2 ) * k * k * k - 2 )
+        -0.5 * ((k -= 2) * k * k * k - 2)
       end
     end
 
@@ -62,23 +62,23 @@ module Moon
     end
 
     QuinticInOut = lambda do |k|
-      if ( ( k *= 2 ) < 1 )
+      if ((k *= 2) < 1)
         0.5 * k * k * k * k * k
       else
-        0.5 * ( ( k -= 2 ) * k * k * k * k + 2 )
+        0.5 * ((k -= 2) * k * k * k * k + 2)
       end
     end
 
     SinusoidalIn = lambda do |k|
-      1 - Math.cos( k * Math::PI / 2 )
+      1 - Math.cos(k * Math::PI / 2)
     end
 
     SinusoidalOut = lambda do |k|
-      Math.sin( k * Math::PI / 2 )
+      Math.sin(k * Math::PI / 2)
     end
 
     SinusoidalInOut = lambda do |k|
-      0.5 * ( 1 - Math.cos( Math::PI * k ) )
+      0.5 * (1 - Math.cos(Math::PI * k))
     end
 
     ExponentialIn = lambda do |k|
@@ -90,11 +90,11 @@ module Moon
     end
 
     ExponentialInOut = lambda do |k|
-      if ( k == 0 )
+      if (k == 0)
         0
-      elsif ( k == 1 )
+      elsif (k == 1)
         1
-      elsif ( ( k *= 2 ) < 1 )
+      elsif ((k *= 2) < 1)
         0.5 * 1024 ** (k - 1)
       else
         0.5 * (-(2 ** (-10 * (k - 1))) + 2)
@@ -102,18 +102,18 @@ module Moon
     end
 
     CircularIn = lambda do |k|
-      1 - Math.sqrt( 1 - k * k )
+      1 - Math.sqrt(1 - k * k)
     end
 
     CircularOut = lambda do |k|
-      Math.sqrt( 1 - ( (k-=1) * k ) )
+      Math.sqrt(1 - ((k-=1) * k))
     end
 
     CircularInOut = lambda do |k|
-      if ( ( k *= 2 ) < 1)
-        -0.5 * ( Math.sqrt( 1 - k * k) - 1)
+      if ((k *= 2) < 1)
+        -0.5 * (Math.sqrt(1 - k * k) - 1)
       else
-        0.5 * ( Math.sqrt( 1 - ( k -= 2) * k) + 1)
+        0.5 * (Math.sqrt(1 - (k -= 2) * k) + 1)
       end
     end
 
@@ -121,90 +121,90 @@ module Moon
       s = nil
       a = 0.1
       p = 0.4
-      if ( k == 0 )
+      if (k == 0)
         0
-      elsif ( k == 1 )
+      elsif (k == 1)
         1
       end
-      if ( !a || a < 1 )
+      if (!a || a < 1)
         a = 1
         s = p / 4
       else
-        s = p * Math.asin( 1 / a ) / ( 2 * Math::PI )
+        s = p * Math.asin(1 / a) / (2 * Math::PI)
       end
-      -( a * (2 ** (10 * (k -= 1))) * Math.sin( ( k - s ) * ( 2 * Math::PI ) / p ) )
+      -(a * (2 ** (10 * (k -= 1))) * Math.sin((k - s) * (2 * Math::PI) / p))
     end
 
     ElasticOut = lambda do |k|
       s = nil
       a = 0.1
       p = 0.4
-      if ( k == 0 )
+      if k == 0
         0
-      elsif ( k == 1 )
+      elsif k == 1
         1
       end
 
-      if ( !a || a < 1 )
+      if a == 0 || a < 1
         a = 1
         s = p / 4
       else
-        s = p * Math.asin( 1 / a ) / ( 2 * Math::PI )
+        s = p * Math.asin(1 / a) / (2 * Math::PI)
       end
-      ( a * (2 ** (-10 * k)) * Math.sin( ( k - s ) * ( 2 * Math::PI ) / p ) + 1 )
+      (a * (2 ** (-10 * k)) * Math.sin((k - s) * (2 * Math::PI) / p) + 1)
     end
 
     ElasticInOut = lambda do |k|
       s = nil
       a = 0.1
       p = 0.4
-      if ( k == 0 )
+      if k == 0
         0
-      elsif ( k == 1 )
+      elsif k == 1
         1
       end
 
-      if ( !a || a < 1 )
+      if a == 0 || a < 1
         a = 1
         s = p / 4
       else
-        s = p * Math.asin( 1 / a ) / ( 2 * Math::PI )
+        s = p * Math.asin(1 / a) / (2 * Math::PI)
       end
-      if ( ( k *= 2 ) < 1 )
-        -0.5 * ( a * (2 ** (10 * ( k -= 1 ))) * Math.sin( ( k - s ) * ( 2 * Math::PI ) / p ) )
+      if (k *= 2) < 1
+        -0.5 * (a * (2 ** (10 * (k -= 1))) * Math.sin((k - s) * (2 * Math::PI) / p))
       else
-        a * (2 ** (-10 * ( k -= 1 )) ) * Math.sin( ( k - s ) * ( 2 * Math::PI ) / p ) * 0.5 + 1
+        a * (2 ** (-10 * (k -= 1))) * Math.sin((k - s) * (2 * Math::PI) / p) * 0.5 + 1
       end
     end
 
     BackIn = lambda do |k|
       s = 1.70158
-      k * k * ( ( s + 1 ) * k - s )
+      k * k * ((s + 1) * k - s)
     end
 
     BackOut = lambda do |k|
       s = 1.70158
-      (k-=1) * k * ( ( s + 1 ) * k + s ) + 1;
+      (k-=1) * k * ((s + 1) * k + s) + 1;
     end
 
     BackInOut = lambda do |k|
       s = 1.70158 * 1.525;
-      if ( ( k *= 2 ) < 1 )
-        0.5 * ( k * k * ( ( s + 1 ) * k - s ) )
+      if ((k *= 2) < 1)
+        0.5 * (k * k * ((s + 1) * k - s))
       else
-        0.5 * ( ( k -= 2 ) * k * ( ( s + 1 ) * k + s ) + 2 )
+        0.5 * ((k -= 2) * k * ((s + 1) * k + s) + 2)
       end
     end
 
     BounceOut = lambda do |k|
-      if ( k < ( 1 / 2.75 ) )
+      if k < (1 / 2.75)
         7.5625 * k * k;
-      elsif ( k < ( 2 / 2.75 ) )
-        7.5625 * ( k -= ( 1.5 / 2.75 ) ) * k + 0.75;
-      elsif ( k < ( 2.5 / 2.75 ) )
-        7.5625 * ( k -= ( 2.25 / 2.75 ) ) * k + 0.9375;
+      elsif k < (2 / 2.75)
+        7.5625 * (k -= (1.5 / 2.75)) * k + 0.75;
+      elsif k < (2.5 / 2.75)
+        7.5625 * (k -= (2.25 / 2.75)) * k + 0.9375;
       else
-        7.5625 * ( k -= ( 2.625 / 2.75 ) ) * k + 0.984375;
+        7.5625 * (k -= (2.625 / 2.75)) * k + 0.984375;
       end
     end
 
@@ -213,10 +213,10 @@ module Moon
     end
 
     BounceInOut = lambda do |k|
-      if ( k < 0.5 )
-        BounceIn.call( k * 2 ) * 0.5
+      if k < 0.5
+        BounceIn.call(k * 2) * 0.5
       else
-        BounceOut.call( k * 2 - 1 ) * 0.5 + 0.5
+        BounceOut.call(k * 2 - 1) * 0.5 + 0.5
       end
     end
   end
--- core/animation/interpolation.rb
+++ core/animation/interpolation.rb
@@ -2,32 +2,32 @@
 module Moon
   module Interpolation
     LinearUtil = lambda do |p0, p1, t|
-      ( p1 - p0 ) * t + p0
+      (p1 - p0) * t + p0
     end
 
     BernsteinUtil = lambda do |n, i|
       fc = UtilFactorial
-      return fc.call( n ) / fc.call( i ) / fc.call( n - i )
+      fc.call(n) / fc.call(i) / fc.call(n - i)
     end
 
     FactorialUtil = lambda do |n|
       a = [1]
       s = 1
-      if ( a[ n ] )
-        a[ n ]
+      if a[n]
+        a[n]
       else
         n.times do |i|
           s *= (n-i)
         end
-        a[ n ] = s
+        a[n] = s
       end
     end
 
     CatmullRomUtil = lambda do |p0, p1, p2, p3, t|
-      v0 = ( p2 - p0 ) * 0.5
-      v1 = ( p3 - p1 ) * 0.5
+      v0 = (p2 - p0) * 0.5
+      v1 = (p3 - p1) * 0.5
       t2 = t * t, t3 = t * t2
-      ( 2 * p1 - 2 * p2 + v0 + v1 ) * t3 + ( - 3 * p1 + 3 * p2 - 2 * v0 - v1 ) * t2 + v0 * t + p1
+      (2 * p1 - 2 * p2 + v0 + v1) * t3 + (-3 * p1 + 3 * p2 - 2 * v0 - v1) * t2 + v0 * t + p1
     end
 
     Linear = lambda do |v, k|
@@ -36,12 +36,12 @@ module Moon
       i = f.floor
       fn = LinearUtil
 
-      if ( k < 0 )
-        fn.call( v[ 0 ], v[ 1 ], f )
-      elsif ( k > 1 )
-        fn.call( v[ m ], v[ m - 1 ], m - f )
+      if k < 0
+        fn.call(v[0], v[1], f)
+      elsif k > 1
+        fn.call(v[m], v[m - 1], m - f)
       end
-      fn.call( v[ i ], v[ i + 1 > m ? m : i + 1 ], f - i )
+      fn.call(v[i], v[i + 1 > m ? m : i + 1], f - i)
     end
 
     Bezier = lambda do |v, k|
@@ -62,18 +62,18 @@ module Moon
       i = f.floor
       fn = CatmullRomUtil
 
-      if ( v[ 0 ] === v[ m ] )
-        if ( k < 0 )
+      if v[0] == v[m]
+        if k < 0
           i = (f = m * (1 + k)).floor
         end
-        fn.call( v[ ( i - 1 + m ) % m ], v[ i ], v[ ( i + 1 ) % m ], v[ ( i + 2 ) % m ], f - i )
+        fn.call(v[(i - 1 + m) % m], v[i], v[(i + 1) % m], v[(i + 2) % m], f - i)
       else
-        if ( k < 0 )
-          v[ 0 ] - ( fn.call( v[ 0 ], v[ 0 ], v[ 1 ], v[ 1 ], -f ) - v[ 0 ] )
-        elsif ( k > 1 )
-          v[ m ] - ( fn.call( v[ m ], v[ m ], v[ m - 1 ], v[ m - 1 ], f - m ) - v[ m ] )
+        if k < 0
+          v[0] - (fn.call(v[0], v[0], v[1], v[1], -f) - v[0])
+        elsif k > 1
+          v[m] - (fn.call(v[m], v[m], v[m - 1], v[m - 1], f - m) - v[m])
         else
-          fn.call( v[ i ? i - 1 : 0 ], v[ i ], v[ m < i + 1 ? m : i + 1 ], v[ m < i + 2 ? m : i + 2 ], f - i )
+          fn.call(v[i ? i - 1 : 0], v[i], v[m < i + 1 ? m : i + 1], v[m < i + 2 ? m : i + 2], f - i)
         end
       end
     end
