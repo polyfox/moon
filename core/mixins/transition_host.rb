770be14b92d9e71964ab83b17608e0a3c8691fee
IceDragon200 <mistdragon100@gmail.com>
2014-07-30 12:48:49 -0500

    namespaced Transitionable and TransitionHost under Moon

--- core/mixins/transition_host.rb
+++ core/mixins/transition_host.rb
@@ -1,56 +1,58 @@
-module TransitionHost
-  ###
-  # @param [Object] src
-  # @param [Object] dest
-  # @param [String] duration
-  # @return [Transition]
-  ###
-  def add_transition(src, dest, duration, &block)
-    duration = Scheduler.parse_duration(duration) if duration.is_a?(String)
-    transition = Transition.new(src, dest, duration, &block)
-    (@transitions ||= []).push transition
-    transition
-  end
-
-  ###
-  # @param [Transition] transition
-  # @return [Void]
-  ###
-  def remove_transition(transition)
-    return unless @transitions
-    @transitions.delete transition
-  end
-
-  ###
-  # @param [Array<Transition>] transitions
-  # @return [Void]
-  ###
-  def remove_transitions(transitions)
-    @transitions -= transitions
-  end
-
-  ###
-  # @param [Float] delta
-  ###
-  def update_transitions(delta)
-    return unless @transitions
-    return if @transitions.empty?
-    dead = []
-    @transitions.each do |transition|
-      transition.update delta
-      dead << transition if transition.done?
+module Moon
+  module TransitionHost
+    ###
+    # @param [Object] src
+    # @param [Object] dest
+    # @param [String] duration
+    # @return [Transition]
+    ###
+    def add_transition(src, dest, duration, &block)
+      duration = Scheduler.parse_duration(duration) if duration.is_a?(String)
+      transition = Transition.new(src, dest, duration, &block)
+      (@transitions ||= []).push transition
+      transition
     end
-    unless dead.empty?
-      remove_transitions dead
-    end
-  end
 
-  ###
-  # Force all transitions to finish.
-  # @return [Void]
-  ###
-  def finish_transitions
-    return unless @transitions
-    @transitions.each(&:finish)
+    ###
+    # @param [Transition] transition
+    # @return [Void]
+    ###
+    def remove_transition(transition)
+      return unless @transitions
+      @transitions.delete transition
+    end
+
+    ###
+    # @param [Array<Transition>] transitions
+    # @return [Void]
+    ###
+    def remove_transitions(transitions)
+      @transitions -= transitions
+    end
+
+    ###
+    # @param [Float] delta
+    ###
+    def update_transitions(delta)
+      return unless @transitions
+      return if @transitions.empty?
+      dead = []
+      @transitions.each do |transition|
+        transition.update delta
+        dead << transition if transition.done?
+      end
+      unless dead.empty?
+        remove_transitions dead
+      end
+    end
+
+    ###
+    # Force all transitions to finish.
+    # @return [Void]
+    ###
+    def finish_transitions
+      return unless @transitions
+      @transitions.each(&:finish)
+    end
   end
 end
--- core/mixins/transitionable.rb
+++ core/mixins/transitionable.rb
@@ -1,57 +1,59 @@
-module Transitionable
+module Moon
+  module Transitionable
 
-  ### mixins
-  include TransitionHost
+    ### mixins
+    include TransitionHost
 
-  ###
-  # @param [String] attribute
-  #   @example "color"
-  #   @example "position.x"
-  # @param [Object] value  target value
-  # @param [Numeric] duration  in seconds
-  ###
-  def transition(attribute, value, duration=0.20)
-    rolling = attribute.to_s.split(".")
-    if rolling.size > 1
-      src = rolling.inject(self) { |obj, param| obj.send(param) }
-      setter = "#{rolling.pop}="
-      add_transition(src, value, duration) do |v|
-        rolling.inject(self) { |obj, param| obj.send(param) }.send(setter, v)
+    ###
+    # @param [String] attribute
+    #   @example "color"
+    #   @example "position.x"
+    # @param [Object] value  target value
+    # @param [Numeric] duration  in seconds
+    ###
+    def transition(attribute, value, duration=0.20)
+      rolling = attribute.to_s.split(".")
+      if rolling.size > 1
+        src = rolling.inject(self) { |obj, param| obj.send(param) }
+        setter = "#{rolling.pop}="
+        add_transition(src, value, duration) do |v|
+          rolling.inject(self) { |obj, param| obj.send(param) }.send(setter, v)
+        end
+      else
+        add_transition(send(attribute), value, duration) { |v| send("#{attribute}=", v) }
       end
-    else
-      add_transition(send(attribute), value, duration) { |v| send("#{attribute}=", v) }
     end
-  end
 
-  ###
-  # Add a keyed transition
-  #   A keyed transition will only have 1 active transition per attribute
-  #   However multiple transition can still operate on the attribute if
-  #   they where not keyed.
-  # @param [String] attribute
-  #   @example "color"
-  #   @example "position.x"
-  # @param [Object] value  target value
-  # @param [Numeric] duration  in seconds
-  ###
-  def key_transition(attribute, value, duration=0.15)
-    @key_transition ||= {}
-    remove_transition(@key_transition[attribute]) if @key_transition.key?(attribute)
-    t = transition(attribute, value, duration)
-    t.key = attribute
-    @key_transition[attribute] = t
-    t
-  end
-
-  ###
-  # @param [Array<Transitions>] transitions
-  # @return [Void]
-  ###
-  def remove_transitions(transitions)
-    if @key_transition
-      transitions.each { |t| @key_transition.delete(t.key) if t.key }
+    ###
+    # Add a keyed transition
+    #   A keyed transition will only have 1 active transition per attribute
+    #   However multiple transition can still operate on the attribute if
+    #   they where not keyed.
+    # @param [String] attribute
+    #   @example "color"
+    #   @example "position.x"
+    # @param [Object] value  target value
+    # @param [Numeric] duration  in seconds
+    ###
+    def key_transition(attribute, value, duration=0.15)
+      @key_transition ||= {}
+      remove_transition(@key_transition[attribute]) if @key_transition.key?(attribute)
+      t = transition(attribute, value, duration)
+      t.key = attribute
+      @key_transition[attribute] = t
+      t
     end
-    super transitions
-  end
 
+    ###
+    # @param [Array<Transitions>] transitions
+    # @return [Void]
+    ###
+    def remove_transitions(transitions)
+      if @key_transition
+        transitions.each { |t| @key_transition.delete(t.key) if t.key }
+      end
+      super transitions
+    end
+
+  end
 end
