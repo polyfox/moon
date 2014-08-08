4a125af4d4e5c195d16cb91af4d8691d1c86fce9
IceDragon200 <mistdragon100@gmail.com>
2014-08-08 16:50:22 -0500

    AABB should be restricted to 2 dimensions
    Use AABBCC for 3d

--- core/aabb.rb
+++ core/aabb.rb
@@ -1,21 +1,25 @@
 # http://studiofreya.com/3d-math-and-physics/simple-aabb-vs-aabb-collision-detection/
 module Moon
   class AABB
-
-    attr_reader :cpos # center position
-    attr_reader :rad  # radii
+    attr_reader :cpos # Vector2  center position
+    attr_reader :rad  # Vector2  radii
 
     def initialize(cpos, rad)
-      @cpos = Vector3[cpos]
-      @rad = Vector3[rad]
+      @cpos = Vector2[cpos]
+      @rad = Vector2[rad]
     end
 
     def intersect?(other)
       return false if (@cpos.x - other.cpos.x).abs > (@rad.x + other.rad.x)
       return false if (@cpos.y - other.cpos.y).abs > (@rad.y + other.rad.y)
-      return false if (@cpos.z - other.cpos.z).abs > (@rad.z + other.rad.z)
       true
     end
 
+    def &(other)
+      rx = (@cpos.x - other.cpos.x)
+      ry = (@cpos.y - other.cpos.y)
+      self.class.new(Vector2.new(@cpos.x + rx/2, @cpos.y + ry/2),
+                     Vector2.new(rx, ry))
+    end
   end
 end
