e968b67c95c3c793e9262a32722e366009812045
IceDragon200 <mistdragon100@gmail.com>
2014-08-31 16:46:33 -0500

    Switched / with - in Base64 random
    Slashes would cause problems when used for filenames

--- core/core_ext/random.rb
+++ core/core_ext/random.rb
@@ -1,11 +1,10 @@
 class Random
-
   BINARY_DIGITS = %w[0 1]
   OCTAL_DIGITS  = %w[0 1 2 3 4 5 6 7]
   HEX_DIGITS    = %w[0 1 2 3 4 5 6 7 8 9 A B C D E F]
   BASE64_DIGITS = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z] +
                   %w[a b c d e f g h i j k l m n o p q r s t u v w x y z] +
-                  %w[0 1 2 3 4 5 6 7 8 9 + /]
+                  %w[0 1 2 3 4 5 6 7 8 9 + -]
 
   def int(size)
     rand(size)
@@ -51,5 +50,4 @@ class Random
   def self.random
     @random ||= new
   end
-
 end
