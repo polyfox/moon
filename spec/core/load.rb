93d967dad9f8e3dc6c00cffa690549e23720ae79
IceDragon200 <mistdragon100@gmail.com>
2014-09-18 15:58:13 -0500

    Remove spec directory

--- spec/core/load.rb
+++
@@ -1,8 +0,0 @@
-$: << "../"
-require "../core/test/micro-spec"
-require "../core/test/micro-assert"
-require "../core/load.rb"
-include Moon::Test
-
-init_test_suite
-init_assertions
--- spec/data_model_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Moon::DataModel::Base do
-  it "should raise a TypeError when given wrong typed objects" do
-    assert_raise(TypeError) { Moon::DataModel::Base.new(id: nil) }
-  end
-
-  it "should accept correctly typed objects" do
-    Moon::DataModel::Base.new(id: "acb635672cdea")
-  end
-end
