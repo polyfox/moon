aac54c12c78f182106acd7ff44ed83156d8d8ab6
Corey Powell <mistdragon100@gmail.com>
2015-03-18 15:12:13 -0500

    Removed old src and include directories

--- include/moon/mrb/engine.hxx
+++
@@ -1,8 +0,0 @@
-#ifndef MMRB_ENGINE_H
-#define MMRB_ENGINE_H
-
-#include "moon/mrb.hxx"
-
-extern struct mrb_data_type engine_data_type;
-
-#endif
--- include/moon/mrb/window.hxx
+++
@@ -1,8 +0,0 @@
-#ifndef MMRB_WINDOW_H
-#define MMRB_WINDOW_H
-
-#include "moon/mrb.hxx"
-
-extern struct mrb_data_type window_data_type;
-
-#endif
