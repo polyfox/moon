ab75637fb54a985a9a1d1dfd210617c3400bd9f7
IceDragon200 <mistdragon100@gmail.com>
2015-05-08 23:42:16 -0500

    Updated mrb_config.rb

--- mrb_config.rb
+++ mrb_config.rb
@@ -1,69 +1,55 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
 
+  # Can I haz sane stack traces please :(
+  #enable_debug # debugging causes oui and blendish to spaz out
+
   # include the default GEMs
   conf.gembox 'default'
+  conf.gembox File.join(File.dirname(__FILE__), 'modules/moon')
 
-  conf.gem core: 'mruby-string-utf8'
   conf.gem core: 'mruby-eval'
+  conf.gem github: 'iij/mruby-io'                # General IO stuff, File system blah de dah.
+  conf.gem github: 'iij/mruby-dir'               # Directory access
+  conf.gem github: 'iij/mruby-regexp-pcre'       # Regular expression engine :3
+  conf.gem github: 'iij/mruby-tempfile'          # required by iij/mruby-require
+  conf.gem github: 'iij/mruby-require'           # require
 
-  conf.gem github: 'iij/mruby-io'
-  conf.gem github: 'iij/mruby-dir'
-  conf.gem github: 'iij/mruby-regexp-pcre'
-  conf.gem github: 'iij/mruby-tempfile'
-  conf.gem github: 'iij/mruby-require'
+  # nice things
+  conf.gem github: 'AndrewBelt/mruby-yaml'       # YAML :3
+  conf.gem github: 'IceDragon200/mruby-nanovg'   # nanovg
+  conf.gem github: 'IceDragon200/mruby-oui'      # OUI
+  conf.gem github: 'IceDragon200/mruby-blendish' # blendish
 
-  # moon gems
   d = File.dirname(__FILE__)
-  # moon-system
-  conf.gem File.join(d, 'modules/system')
-  # moon-graphics
-  conf.gem File.join(d, 'modules/graphics')
-  # moon-audio
-  conf.gem File.join(d, 'modules/audio')
-  # moon-engine
-  conf.gem File.join(d, 'modules/engine')
-
-  # so we can have nice things
-  conf.gem github: 'AndrewBelt/mruby-yaml'
-  conf.gem github: 'IceDragon200/mruby-glfw3'
-  conf.gem github: 'IceDragon200/mruby-glew'
-  conf.gem github: 'IceDragon200/mruby-nanovg'
-  conf.gem github: 'IceDragon200/mruby-oui'
-  conf.gem github: 'IceDragon200/mruby-blendish'
-
   [conf.cc, conf.cxx].each do |cc|
     # system
     cc.include_paths << File.join(d, 'vendor/glm')
     # graphics
     cc.include_paths << File.join(d, 'vendor/glfw/include')
-    cc.include_paths << File.join(d, 'vendor/nanovg/src')
-    cc.include_paths << File.join(d, 'vendor/oui-blendish')
     cc.include_paths << File.join(d, 'vendor/soil/include')
     cc.include_paths << File.join(d, 'vendor/sil/include')
     cc.include_paths << File.join(d, 'vendor/freetype-gl')
+    cc.include_paths << File.join(d, 'vendor/nanovg/src')
+    cc.include_paths << File.join(d, 'vendor/oui-blendish')
     # audio
     cc.include_paths << File.join(d, 'vendor/gorilla-audio/include')
-    # moon include
-    cc.include_paths << File.join(d, 'modules/system/include')
-    cc.include_paths << File.join(d, 'modules/graphics/include')
-    cc.include_paths << File.join(d, 'modules/audio/include')
   end
 
   conf.linker do |l|
-    linker.library_paths << File.join(d, 'vendor/nanovg/build')
-    linker.library_paths << File.join(d, 'vendor/soil/build')
-    linker.library_paths << File.join(d, 'vendor/sil/build')
     linker.library_paths << File.join(d, 'vendor/freetype-gl/build')
-    linker.library_paths << File.join(d, 'vendor/gorilla-audio/build/build')
     linker.library_paths << File.join(d, 'vendor/glfw/build')
+    linker.library_paths << File.join(d, 'vendor/gorilla-audio/build/build')
+    linker.library_paths << File.join(d, 'vendor/nanovg/build')
+    linker.library_paths << File.join(d, 'vendor/sil/build')
+    linker.library_paths << File.join(d, 'vendor/soil/build')
     linker.libraries << 'pthread'
     linker.libraries << 'GL'
     linker.libraries << 'openal'
     linker.libraries << 'glfw'
-    linker.libraries << 'nanovg'
     linker.libraries << 'SOIL'
     linker.libraries << 'SIL'
+    linker.libraries << 'nanovg'
     linker.libraries << 'freetype'
     linker.libraries << 'freetype-gl'
     linker.libraries << 'gorilla'
