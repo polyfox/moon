c958aa439eb45d97a943ca1e2e464947b55145b5
IceDragon200 <mistdragon100@gmail.com>
2015-05-10 14:33:04 -0500

    Updated mrb_config.rb
    if on darwin, default to GLFW as the GL header loader, else use GLEW

--- modules/graphics/include/moon/gl.h
+++ modules/graphics/include/moon/gl.h
@@ -2,14 +2,16 @@
 #define MOON_GL_H
 
 /* Some platforms will still require GLEW to be loaded */
-#ifdef MOON_GLEW
+#ifdef MOON_GL_GLEW
 #  include <GL/glew.h>
 #endif
 
 /* OpenGL via GLFW */
-#define GLFW_INCLUDE_GLCOREARB
-#define GLFW_INCLUDE_GLEXT
-#define GL3_PROTOTYPES
-#include <GLFW/glfw3.h>
+#ifdef MOON_GL_GLFW
+#  define GLFW_INCLUDE_GLCOREARB
+#  define GLFW_INCLUDE_GLEXT
+#  define GL3_PROTOTYPES
+#  include <GLFW/glfw3.h>
+#endif
 
 #endif
--- mrb_config.rb
+++ mrb_config.rb
@@ -26,7 +26,7 @@ MRuby::Build.new do |conf|
   conf.gem github: 'IceDragon200/mruby-glfw3'    # GLFW
   conf.gem github: 'IceDragon200/mruby-nanovg'   # nanovg
 
-  conf.cxx do |cxx|
+  conf.cxx do |c|
     std = 'c++11'
     comp = toolchain_name == :clang ? 'clang' : 'gcc'
     result = `#{comp} -dumpversion`.chomp
@@ -41,50 +41,54 @@ MRuby::Build.new do |conf|
       end
     end
     puts "\t\\\\ Using #{comp}(#{result}) c++ std: #{std} \\\\"
-    cxx.flags << "-std=#{std}"
+    c.flags << "-std=#{std}"
   end
 
   d = File.dirname(__FILE__)
   vd = File.expand_path('vendor', d)
-  [conf.cc, conf.cxx].each do |cc|
-    cc.flags << '-g3'
+  [conf.cc, conf.cxx].each do |c|
+    c.flags << '-g3'
 
-    cc.flags << '-DMOON_GLEW' if Platform.linux?
+    if Platform.darwin?
+      c.flags << '-DMOON_GL_GLFW'
+    else
+      c.flags << '-DMOON_GL_GLEW'
+    end
 
     # system
-    cc.include_paths << File.join(vd, 'glm')
+    c.include_paths << File.join(vd, 'glm')
     # graphics
-    cc.include_paths << File.join(vd, 'glfw/include')
-    cc.include_paths << File.join(vd, 'soil/include')
-    cc.include_paths << File.join(vd, 'sil/include')
-    cc.include_paths << File.join(vd, 'freetype-gl')
-    cc.include_paths << File.join(vd, 'nanovg/src')
+    c.include_paths << File.join(vd, 'glfw/include')
+    c.include_paths << File.join(vd, 'soil/include')
+    c.include_paths << File.join(vd, 'sil/include')
+    c.include_paths << File.join(vd, 'freetype-gl')
+    c.include_paths << File.join(vd, 'nanovg/src')
     # audio
-    cc.include_paths << File.join(vd, 'gorilla-audio/include')
+    c.include_paths << File.join(vd, 'gorilla-audio/include')
   end
 
   conf.linker do |l|
-    linker.library_paths << File.join(vd, 'glfw/build/src')
-    linker.library_paths << File.join(vd, 'freetype-gl/build')
-    linker.library_paths << File.join(vd, 'gorilla-audio/build/build')
-    linker.library_paths << File.join(vd, 'nanovg/build')
-    linker.library_paths << File.join(vd, 'sil/build')
-    linker.library_paths << File.join(vd, 'soil/build')
-    linker.libraries << 'glfw'
-    linker.libraries << 'freetype-gl'
-    linker.libraries << 'gorilla'
-    linker.libraries << 'nanovg'
-    linker.libraries << 'freetype'
-    linker.libraries << 'SOIL'
-    linker.libraries << 'SIL'
+    l.library_paths << File.join(vd, 'glfw/build/src')
+    l.library_paths << File.join(vd, 'freetype-gl/build')
+    l.library_paths << File.join(vd, 'gorilla-audio/build/build')
+    l.library_paths << File.join(vd, 'nanovg/build')
+    l.library_paths << File.join(vd, 'sil/build')
+    l.library_paths << File.join(vd, 'soil/build')
+    l.libraries << 'glfw'
+    l.libraries << 'freetype-gl'
+    l.libraries << 'gorilla'
+    l.libraries << 'nanovg'
+    l.libraries << 'freetype'
+    l.libraries << 'SOIL'
+    l.libraries << 'SIL'
     if Platform.darwin?
-      linker.flags_after_libraries << '-framework OpenGL'
-      linker.flags_after_libraries << '-framework OpenAL'
-      linker.flags_after_libraries << '-framework CoreFoundation'
+      l.flags_after_libraries << '-framework OpenGL'
+      l.flags_after_libraries << '-framework OpenAL'
+      l.flags_after_libraries << '-framework CoreFoundation'
     else
-      linker.libraries << 'GL'
-      linker.libraries << 'openal'
+      l.libraries << 'GL'
+      l.libraries << 'openal'
     end
-    linker.libraries << 'pthread'
+    l.libraries << 'pthread'
   end
 end
