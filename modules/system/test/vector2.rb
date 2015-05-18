c4080f3f21f679c4d30088f114ae9d8c4cfb5b33
IceDragon200 <mistdragon100@gmail.com>
2015-05-17 20:01:55 -0500

    Added Vector2#modulo test

--- modules/system/test/vector2.rb
+++ modules/system/test/vector2.rb
@@ -16,3 +16,13 @@ assert 'Moon::Vector2#[]' do
   assert_equal [1, 2], Moon::Vector2[Moon::Vector1.new(1), Moon::Vector1.new(2)].to_a
   assert_equal [1, 2], Moon::Vector2[Moon::Vector2.new(1, 2)].to_a
 end
+
+assert 'Moon::Vector2#modulo with num' do
+  v = Moon::Vector2.new(8, 4)
+  assert_equal [2, 1], (v % 3).to_a
+end
+
+assert 'Moon::Vector2#modulo with Vector2' do
+  v = Moon::Vector2.new(8, 4)
+  assert_equal [2, 1], (v % Moon::Vector2.new(3, 3)).to_a
+end
