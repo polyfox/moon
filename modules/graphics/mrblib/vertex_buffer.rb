4432d2d4b6162c6c77344171b9a63da4ab915938
IceDragon200 <mistdragon100@gmail.com>
2015-06-24 18:45:42 -0500

    graphics: Constant aliases for VertexBuffer DRAW types

---
+++ modules/graphics/mrblib/vertex_buffer.rb
@@ -0,0 +1,9 @@
+module Moon
+  class VertexBuffer
+    # @!group Buffer types
+    DYNAMIC_DRAW = GL2::GL_DYNAMIC_DRAW
+    STATIC_DRAW  = GL2::GL_STATIC_DRAW
+    STREAM_DRAW  = GL2::GL_STREAM_DRAW
+    # @!endgroup
+  end
+end
