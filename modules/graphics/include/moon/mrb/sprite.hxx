0fb03b5b4c452781ba80676023f46eeced400098
IceDragon200 <mistdragon100@gmail.com>
2015-07-01 09:27:47 -0500

    sprite and spritesheet data_types are no longer needed

--- modules/graphics/include/moon/mrb/sprite.hxx
+++ modules/graphics/include/moon/mrb/sprite.hxx
@@ -6,7 +6,6 @@
 #include <mruby/data.h>
 #include "moon/api.h"
 
-MOON_C_API const struct mrb_data_type sprite_data_type;
 MOON_C_API void mmrb_sprite_init(mrb_state *mrb, struct RClass* mod);
 
 #endif
--- modules/graphics/include/moon/mrb/spritesheet.hxx
+++ modules/graphics/include/moon/mrb/spritesheet.hxx
@@ -6,7 +6,6 @@
 #include <mruby/data.h>
 #include "moon/api.h"
 
-MOON_C_API const struct mrb_data_type spritesheet_data_type;
 MOON_C_API void mmrb_spritesheet_init(mrb_state *mrb, struct RClass* mod);
 
 #endif
