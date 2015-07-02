7bedc01d86811596cfa7167192a7746e8e02d0e3
IceDragon200 <mistdragon100@gmail.com>
2015-07-02 12:12:45 -0500

    Implemented OSX and Windows moon_get_current_time

--- modules/system/include/moon/time.hxx
+++ modules/system/include/moon/time.hxx
@@ -2,45 +2,20 @@
 #define MOON_TIME_H
 
 #include <stdint.h>
+#include "moon/api.h"
 
-// Time here is returned in microseconds
-
-// http://stackoverflow.com/questions/5919996/how-to-detect-reliably-mac-os-x-ios-linux-windows-in-c-preprocessor
 #ifdef _WIN32
-#  include <windows.h>
-
-static inline uint64_t
-moon_get_current_time()
-{
-  // TODO: implement
-}
-
-#elif __APPLE__
-#  include <mach/mach.h>
-#  include <mach/mach_time.h>
-
-static inline uint64_t
-moon_get_current_time()
-{
-  // TODO: implement
-}
-
+MOON_C_API unsigned __int64 moon_get_current_time();
 #else
-#  include <time.h>
-
-static inline uint64_t
-moon_get_current_time()
-{
-  struct timespec spec;
-  clock_gettime(CLOCK_MONOTONIC, &spec);
-  return spec.tv_sec * 1000000 + spec.tv_nsec / 1000;
-}
-
+MOON_C_API uint64_t moon_get_current_time();
 #endif
+
+void moon_time_init();
 
 static inline float
 moon_microseconds_to_seconds(uint64_t mics)
 {
   return static_cast<float>(mics / 1000000);
 }
+
 #endif
