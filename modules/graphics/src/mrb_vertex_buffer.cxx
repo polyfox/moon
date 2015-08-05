16d2a5f480eac68edbdbb4197485dd932418e649
IceDragon200 <mistdragon100@gmail.com>
2015-08-05 10:30:26 -0500

    Added VertexBuffer#add_quad

--- modules/graphics/src/mrb_vertex_buffer.cxx
+++ modules/graphics/src/mrb_vertex_buffer.cxx
@@ -3,6 +3,10 @@
 #include <mruby/data.h>
 #include <mruby/numeric.h>
 #include <mruby/string.h>
+#include "moon/mrb/rect.hxx"
+#include "moon/mrb/vector2.hxx"
+#include "moon/mrb/vector3.hxx"
+#include "moon/mrb/vector4.hxx"
 #include "moon/mrb/vertex_buffer.hxx"
 #include "moon/mrb/helpers.hxx"
 #include "moon/vertex_buffer.hxx"
@@ -95,6 +99,53 @@ vbo_index_count(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(mmrb_vertex_buffer_ptr(mrb, self)->GetIndexCount());
 }
 
+static mrb_value
+vbo_add_quad(mrb_state *mrb, mrb_value self)
+{
+  Moon::IntRect quad_rect;
+  Moon::FloatRect quad_texture_rect;
+  Moon::Vector4 color;
+  Moon::VertexBuffer *vbo = NULL;
+  Moon::Vertex vertices[4];
+  GLuint indices[] = { 0, 1, 3, 2, 3, 1 };
+
+  mrb_value rect_obj;
+  mrb_value tex_rect_obj;
+  mrb_value color_obj;
+  mrb_int i = 0;
+
+  mrb_get_args(mrb, "ooo", &rect_obj, &tex_rect_obj, &color_obj);
+  quad_rect = mmrb_to_rect(mrb, rect_obj);
+  quad_texture_rect = mmrb_to_float_rect(mrb, tex_rect_obj);
+  color = mmrb_to_vector4(mrb, tex_rect_obj);
+  vbo = mmrb_vertex_buffer_ptr(mrb, self);
+
+  printf("quad: x=%i y=%i w=%i h=%i\n", quad_rect.x, quad_rect.y, quad_rect.w, quad_rect.h);
+  printf("tex_quad: x=%f y=%f w=%f h=%f\n", quad_texture_rect.x, quad_texture_rect.y, quad_texture_rect.w, quad_texture_rect.h);
+
+  vertices[i].pos = Moon::Vector2(quad_rect.x, quad_rect.y);
+  vertices[i].tex_coord = Moon::Vector2(quad_texture_rect.x, quad_texture_rect.y);
+  vertices[i].color = color;
+
+  i++;
+  vertices[i].pos = Moon::Vector2(quad_rect.x + quad_rect.w, quad_rect.y);
+  vertices[i].tex_coord = Moon::Vector2(quad_texture_rect.x + quad_texture_rect.w, quad_texture_rect.y);
+  vertices[i].color = color;
+
+  i++;
+  vertices[i].pos = Moon::Vector2(quad_rect.x + quad_rect.w, quad_rect.y + quad_rect.h);
+  vertices[i].tex_coord = Moon::Vector2(quad_texture_rect.x + quad_texture_rect.w, quad_texture_rect.y + quad_texture_rect.h);
+  vertices[i].color = color;
+
+  i++;
+  vertices[i].pos = Moon::Vector2(quad_rect.x, quad_rect.y + quad_rect.h);
+  vertices[i].tex_coord = Moon::Vector2(quad_texture_rect.x, quad_texture_rect.y + quad_texture_rect.h);
+  vertices[i].color = color;
+
+  vbo->PushBack(vertices, 4, indices, 6);
+  return self;
+}
+
 MOON_C_API void
 mmrb_vbo_init(mrb_state *mrb, struct RClass* mod)
 {
@@ -108,7 +159,7 @@ mmrb_vbo_init(mrb_state *mrb, struct RClass* mod)
   mrb_define_method(mrb, vbo_class, "push_indices", vbo_push_indices, MRB_ARGS_REQ(1));
   mrb_define_method(mrb, vbo_class, "vertex_count", vbo_vertex_count, MRB_ARGS_NONE());
   mrb_define_method(mrb, vbo_class, "index_count",  vbo_index_count,  MRB_ARGS_NONE());
-
+  mrb_define_method(mrb, vbo_class, "add_quad",     vbo_add_quad,     MRB_ARGS_REQ(3));
   // vbo.render(mode, {offset: [default: 0]})
   // vbo.flush // vbo.upload
 
