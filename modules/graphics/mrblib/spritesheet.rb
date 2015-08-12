682c66c7cf4f7b1d31174c8c97d15e91e8b7f83d
IceDragon200 <mistdragon100@gmail.com>
2015-08-12 11:21:54 -0500

    Moved Moon::Spritesheet#render to mruby
    Also the rendering is broken, we might need to change the mode to TRIANGLES and generate the vbo using 6 vertices instead of 4 like with VertexBuffer#add_quad

--- modules/graphics/mrblib/spritesheet.rb
+++ modules/graphics/mrblib/spritesheet.rb
@@ -27,7 +27,31 @@ module Moon
       @cell_count = 0
       @shader = self.class.default_shader
       @vbo = VertexBuffer.new(VertexBuffer::STATIC_DRAW)
+      @transform = Matrix4.new
+      @rotation_matrix = Matrix4.new
+      @mode = OpenGL::TRIANGLE_STRIP
       generate_buffers
+    end
+
+    def render(x, y, z, index, options = {})
+      angle = options.fetch(:angle, 0.0)
+      origin = options.fetch(:origin) { Moon::Vector2.new(0, 0) }
+      opacity = options.fetch(:opacity, 1.0)
+      color = options.fetch(:color, 1.0) { Moon::Vector4.new(1, 1, 1, 1) }
+      tone = options.fetch(:tone, 1.0) { Moon::Vector4.new(0, 0, 0, 1) }
+
+      @rotation_matrix.clear
+      @rotation_matrix.rotate!(angle, [0, 0, 1])
+      @rotation_matrix.translate!(-origin.x, -origin.y, 0)
+      @transform.clear
+      @transform.translate!(x, y, z)
+      transform = @transform * @rotation_matrix
+
+      @shader.use
+      @shader.set_uniform 'opacity', opacity
+      @shader.set_uniform 'color', color
+      @shader.set_uniform 'tone', tone
+      Renderer.instance.render(@shader, @vbo, @texture, transform, @mode, index)
     end
   end
 end
