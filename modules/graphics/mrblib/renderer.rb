e47f6649cf405e8bf7efd53b28765ef50c94d22e
IceDragon200 <mistdragon100@gmail.com>
2015-08-12 10:34:57 -0500

    Added index parameter to Moon::Renderer#render

--- modules/graphics/mrblib/renderer.rb
+++ modules/graphics/mrblib/renderer.rb
@@ -1,9 +1,10 @@
 module Moon
   class Renderer
-    def render(shader, vbo, texture, transform, mode)
+    def render(shader, vbo, texture, transform, mode, index = 0)
       apply_transform shader, transform
       bind_texture shader, texture
-      vbo.render mode
+      vbo.render mode, index
+    end
     end
   end
 end
