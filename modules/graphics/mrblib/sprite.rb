11fc4a187b4d0b6600528742afbbc00ebdf41688
IceDragon200 <mistdragon100@gmail.com>
2015-08-12 10:36:14 -0500

    Moved Sprite#render to mruby

--- modules/graphics/mrblib/sprite.rb
+++ modules/graphics/mrblib/sprite.rb
@@ -31,6 +31,9 @@ module Moon
 
       @shader = self.class.default_shader
       @vbo = VertexBuffer.new(VertexBuffer::DYNAMIC_DRAW)
+      @transform = Matrix4.new
+      @rotation_matrix = Matrix4.new
+      @mode = OpenGL::TRIANGLE_STRIP
       generate_buffers
     end
 
@@ -44,6 +47,21 @@ module Moon
     def clip_rect= rect
       set_clip_rect rect
       generate_buffers
+    end
+
+    def render(x, y, z)
+      @rotation_matrix.clear
+      @rotation_matrix.rotate!(@angle, [0, 0, 1])
+      @rotation_matrix.translate!(-@origin.x, -@origin.y, 0)
+      @transform.clear
+      @transform.translate!(x, y, z)
+      transform = @transform * @rotation_matrix
+
+      @shader.use
+      @shader.set_uniform 'opacity', @opacity
+      @shader.set_uniform 'color', @color
+      @shader.set_uniform 'tone', @tone
+      Renderer.instance.render(@shader, @vbo, @texture, transform, @mode)
     end
   end
 end
