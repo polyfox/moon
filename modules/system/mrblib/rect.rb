60b55c83a05c3ce446b41c31e4756b63a1239786
IceDragon200 <mistdragon100@gmail.com>
2015-09-16 13:35:56 -0500

    Added Rect#clear, and Rect#to_s, as well as comments

--- modules/system/mrblib/rect.rb
+++ modules/system/mrblib/rect.rb
@@ -1,9 +1,30 @@
 module Moon
   class Rect
+    # Resets all the properties to 0
+    #
+    # @return [self]
+    def clear
+      self.x, self.y, self.w, self.h = 0, 0, 0, 0
+      self
+    end
+
+    # Returns the Rect's properties in an Array
+    #
+    # @return [Array<Integer>]
     def to_a
       [x, y, w, h]
     end
 
+    # @return [String]
+    def to_s
+      "#{x},#{y},#{w},#{h}"
+    end
+
+    # Compares the Rect against another object, if the other object is a Rect,
+    # it will compare their properties, else the result is always false.
+    #
+    # @param [Object] other
+    # @return [Boolean]
     def ==(other)
       if other.is_a?(Moon::Rect)
         x == other.x && y == other.y && w == other.w && h == other.h
