fddc9394ea9c8294b7c412239bad364caec232c2
Blaž Hrastnik <blaz.hrast@gmail.com>
2016-04-11 11:54:48 +0900

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew update || exit
-  for package in libyaml freetype glfw3 cmake ; do
-    brew outdated "${package}" || brew upgrade "${package}" || exit
-  done
-
-  whereis libfreetype
+  brew upgrade libyaml cmake
+  brew install freetype glfw3
   brew link freetype
 fi
 
