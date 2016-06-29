873c9f75f96941fe91c601b2355459472cb0b3f9
IceDragon200 <mistdragon100@gmail.com>
2016-06-29 14:02:43 -0500

    modules/audio-libsoundio: Added SoundBuffer#sampleRate
    I totally forgot about it

--- modules/audio-libsoundio/mrblib/sound_buffer.rb
+++ modules/audio-libsoundio/mrblib/sound_buffer.rb
@@ -1,12 +1,15 @@
 module Moon
   class SoundBuffer
+    private :initialize_create
+    private :initialize_by_filename
+
     def initialize(*args)
-      if args.size == 2
-        initialize_create(*args)
-      elsif args.size == 1
+      if args.size == 1
         initialize_by_filename(args.first)
+      elsif args.size >= 2
+        initialize_create(*args)
       else
-        raise ArgumentError, "expected wrong argument count, (expected 1 or 2)"
+        raise ArgumentError, "expected wrong argument count, (expected 1, 2 or 3)"
       end
     end
   end
