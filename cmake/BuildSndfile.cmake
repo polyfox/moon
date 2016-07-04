35ff5fd11fb816f44093d29cdde4fd08faff98e2
Blaž Hrastnik <blaz.hrast@gmail.com>
2016-07-05 02:51:10 +0900

    Don't disable external libs...

--- cmake/BuildSndfile.cmake
+++ cmake/BuildSndfile.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(libsndfile
   URL http://www.mega-nerd.com/libsndfile/files/${SNDFILE_TARBALL}.tar.gz
   BUILD_IN_SOURCE 1
   INSTALL_DIR vendor/libsndfile
-  CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/vendor/libsndfile --disable-external-libs --disable-shared
+  CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/vendor/libsndfile --disable-shared
   BUILD_COMMAND $(MAKE) V=1
   INSTALL_COMMAND $(MAKE) install
 )
