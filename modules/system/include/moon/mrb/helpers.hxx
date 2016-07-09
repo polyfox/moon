2f001af6e111b21d1bed8f78b25c05ebcdf16722
IceDragon200 <mistdragon100@gmail.com>
2016-07-09 13:37:16 -0500

    modules/audio-libsoundio: Stubbed Music

--- modules/system/include/moon/mrb/helpers.hxx
+++ modules/system/include/moon/mrb/helpers.hxx
@@ -27,4 +27,14 @@ moon_iv_get(mrb_state *mrb, mrb_value self, const char *name)
   return iv_value;
 }
 
+static inline void
+moon_data_cleanup(mrb_state *mrb, mrb_value self)
+{
+  void *ptr = DATA_PTR(self);
+  const struct mrb_data_type* type = DATA_TYPE(self);
+  if (ptr && type) {
+    type->dfree(mrb, ptr);
+  }
+  DATA_PTR(self) = NULL;
+}
 #endif
