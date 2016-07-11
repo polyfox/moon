1bf51707c8c362bb1b60974db7f0867067c04bae
Blaž Hrastnik <blaz.hrast@gmail.com>
2016-07-11 22:31:05 +0900

    Example overload that doesn't work in mrb source

--- modules/system/mrblib/vector3.rb
+++ modules/system/mrblib/vector3.rb
@@ -2,6 +2,11 @@ module Moon
   class Vector3
     include Comparable
 
+    # @!method ^(other)
+    #   Add two vectors together.
+    #   @param [Vector3] other
+    #   @return [Vector3]
+
     # @!attribute x
     #  @return [Integer] x coordinate
     # @!attribute y
